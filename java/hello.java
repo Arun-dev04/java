1a26fa7576d466fe9bf7e8aae4aae4c55077825e
Arun Singh Sengar <arunsinghsengar2004@gmail.com>
2024-09-19 21:17:37 +0530

    JAva projects

--- java/hello.java
+++ java/hello.java
@@ -1,15 +1,6 @@
-import java.util.Scanner;
-
 public class hello {
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        float d= scan.nextFloat();
-        String s = scan.nextLine();
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+             System.out.println("Hello World!");
+     
     }
 }
